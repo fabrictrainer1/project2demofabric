a943a24d2cbbb210eab01c446b7c53058deba33c
fabrictrainer1 <fabrictrainer@ytta.in>
2025-02-24 10:35:12 +0530

    Committing 1 item from workspace 0ecd635e-e159-4e01-ba99-201d728cdf8d

---
+++ Transform notebook.Notebook/notebook-content.py
@@ -0,0 +1,20 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "8ced2574-80f0-4ddc-90f6-0d09a93c2e2b",
+# META       "default_lakehouse_name": "data_lakehouse",
+# META       "default_lakehouse_workspace_id": "0ecd635e-e159-4e01-ba99-201d728cdf8d"
+# META     }
+# META   }
+# META }
+
+# MARKDOWN ********************
+
+# #This is transformation notebook.
